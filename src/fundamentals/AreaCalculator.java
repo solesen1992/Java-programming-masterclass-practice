52d3f0c21ff13d8bf26d844b8414258352a4718f
solesen1992 <saramundal@live.dk>
2024-08-14 13:22:16 +0200

    Update AreaCalculator

--- src/fundamentals/AreaCalculator.java
+++ src/fundamentals/AreaCalculator.java
@@ -17,11 +17,12 @@ public class AreaCalculator {
     
     // Area of a circle
     public static double area (double radius) {
-        if (radius >=0 ) {
+        if (radius >= 0 ) {
             double areaOfCircle = radius * radius * Math.PI;
             System.out.println("Area of circle: " + areaOfCircle);
             return areaOfCircle;
         } else {
+        	System.out.println("Invalid Value: -1");
             return -1.0;
         }
     }
@@ -33,7 +34,33 @@ public class AreaCalculator {
             System.out.println("Area of rectangle: " + areaOfRectangle);
             return areaOfRectangle;
         } else {
+        	System.out.println("Invalid Value: -1");
             return -1.0;
         }
     }
 }
+
+
+/**
+ * ALTERNATIVE SOLUTION
+ * public class AreaCalculator {
+ 
+    private static final int INVALID_VALUE = -1;
+ 
+    public static double area(double radius) {
+        
+        if (radius < 0) {
+            return INVALID_VALUE;
+        }
+        return radius * radius * Math.PI;
+    }
+ 
+    public static double area(double x, double y) {
+        
+        if (x < 0 || y < 0) {
+            return INVALID_VALUE;
+        }
+        return x * y;
+    }
+}
+ * */
