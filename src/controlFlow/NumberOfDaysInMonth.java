180e5c1bf4c6340416c731baa6b8db2d1d01a144
solesen1992 <saramundal@live.dk>
2024-08-15 14:48:16 +0200

    Create NumberOfDaysInMonth

---
+++ src/controlFlow/NumberOfDaysInMonth.java
@@ -0,0 +1,10 @@
+package controlFlow;
+
+/**
+ * The objective of this coding exercise is to write a small program 
+ * that determines the number of days in a particular month.
+ * */
+
+public class NumberOfDaysInMonth {
+
+}
