c0e6583bc03c6171c034ebd6a76d94c4e1fbbd47
solesen1992 <saramundal@live.dk>
2024-08-22 16:00:56 +0200

    Adding testing in main method

--- src/controlFlow/NumberToWords.java
+++ src/controlFlow/NumberToWords.java
@@ -9,7 +9,25 @@ public class NumberToWords {
 	
 	// To test method
 	public static void main(String[] args) {
-		
+		//getDigitCount method
+		getDigitCount(0); //should return 1 since there is only 1 digit
+		getDigitCount(123); //should return 3
+		getDigitCount(-12); //should return -1 since the parameter is negative
+		getDigitCount(5200); //should return 4 since there are 4 digits in the number
+
+		//reverse method
+		reverse(-121); //should  return -121
+		reverse(1212); //should return  2121
+		reverse(1234); //should return 4321
+		reverse(100); //should return 1
+
+		//numberToWords method
+		numberToWords(123); //should print "One Two Three".
+		numberToWords(1010); //should print "One Zero One Zero".
+		numberToWords(1000); //should print "One Zero Zero Zero".
+		numberToWords(-12); //should print "Invalid Value" since the parameter is negative.
 	}
+	
+	
 
 }
