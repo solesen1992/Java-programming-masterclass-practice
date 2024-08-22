188f086ad81d5311186de9b9e75b5a402689fd1a
solesen1992 <saramundal@live.dk>
2024-08-22 15:58:24 +0200

    Create NumberToWords

---
+++ src/controlFlow/NumberToWords.java
@@ -0,0 +1,15 @@
+package controlFlow;
+
+/**
+ * The objective of this coding exercise is to write a small program 
+ * that prints out the number in words.
+ * */
+
+public class NumberToWords {
+	
+	// To test method
+	public static void main(String[] args) {
+		
+	}
+
+}
