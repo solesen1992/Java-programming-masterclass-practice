3878662a4d119d724543225295259e6a1aefefd2
solesen1992 <saramundal@live.dk>
2024-08-24 12:11:50 +0200

    Create DiagonalStar

---
+++ src/controlFlow/DiagonalStar.java
@@ -0,0 +1,12 @@
+package controlFlow;
+
+/**
+ * The objective of this coding exercise is to create a simple program 
+ * that generates a rectangular pattern consisting of stars (*) to form diagonal lines.
+ * */
+
+public class DiagonalStar {
+	
+	
+
+}
